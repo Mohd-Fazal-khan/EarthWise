81179e86964e56113069f035309503a84b28d677
Fazal Khan <124332727+Mohd-Fazal-khan@users.noreply.github.com>
2025-09-03 17:18:49 +0530

    Update firebaseconfig.ts

--- firebase/firebaseconfig.ts
+++ firebase/firebaseconfig.ts
@@ -8,12 +8,12 @@ import { getFirestore } from "firebase/firestore";
 
 // Firebase config
 const firebaseConfig = {
-  apiKey: "AIzaSyCyb2ehx_Mx3-TyEH92D124RXhrj3Y9C4s",
-  authDomain: "earthwise-7c8fd.firebaseapp.com",
-  projectId: "earthwise-7c8fd",
-  storageBucket: "earthwise-7c8fd.appspot.com",
-  messagingSenderId: "762776486043",
-  appId: "1:762776486043:web:36b7c59b654503e7d1063e",
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
 };
 
 // Initialize Firebase
